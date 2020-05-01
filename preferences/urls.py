e144fbe7afeb613f78ed2f7befaa4f01963a953a
rkasale28 <rohitkasale28@gmail.com>
2020-05-01 09:39:47 +0530

    Designed all pages

--- preferences/urls.py
+++ preferences/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from preferences import views
 
 urlpatterns = [
-    path('fp', views.fp, name='home'),
+    path('fp', views.fp, name='fp'),
+    path('sp', views.sp, name='sp'),
+    path('tp', views.tp, name='tp'),
+    path('fop', views.fop, name='fop'),
+    path('fip', views.fip, name='fip'),
+    path('sip', views.sip, name='sip'),
 ]
--- preferences/views.py
+++ preferences/views.py
@@ -57,3 +57,258 @@ def fp(request):
     missed=(140-included)
     
     return render(request, 'fp_home.html',{'missed':missed,'included':included,'titles':titles})
+
+def sp(request):
+    df=pd.read_excel('static/excel/IDC.xlsx',sheet_name='SP')
+    x_test=np.array([140]).reshape(-1,1)
+    y_pred=[]
+
+    X=np.array(df['No. of responses'])
+    Y=[np.array(df['DIP']),np.array(df['AIS']),np.array(df['SEO']),np.array(df['SA']),np.array(df['UE']),np.array(df['ACN'])]
+
+    titles=['DIP','AIS','SEO','SA','UE','ACN']
+    x1=X.tolist()
+    num=0
+    for y in Y:
+        reg = LinearRegression().fit(X.reshape(-1,1), y)
+        p=reg.predict(x_test)
+        y_pred.append(round(p[0],0))
+        y1=y.tolist()
+
+        fig = plt.figure(figsize=(6,4))
+        plt.plot(x1, y1,'o-b')
+        
+        plt.title(titles[num])
+
+        plt.xticks([15,25,35,45,55,65])
+        num+=1
+
+        plt.xlabel('Total No. of Students')
+        plt.ylabel('Students in favour')
+        
+        for i,j in zip(X,y):
+            plt.annotate(str(round(j,0)),xy=(i,j))
+        plt.tight_layout()
+        fig.savefig('static/images/sp/'+titles[num-1]+'.jpg')
+    
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.bar(titles, y_pred, color = 'b')
+    plt.yticks([0,50,100])
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x()+(p.get_width()/2), p.get_height() * 1.01))
+
+    plt.xlabel('Subject')
+    plt.ylabel('Expected no. of Students in favour')
+    plt.title('Prediction for total 140 students')
+        
+    plt.tight_layout()
+    fig.savefig('static/images/sp/analysis.jpg')
+
+    included=sum(y_pred)
+    missed=(140-included)
+    
+    return render(request, 'sp_home.html',{'missed':missed,'included':included,'titles':titles})
+
+def tp(request):
+    df=pd.read_excel('static/excel/IDC.xlsx',sheet_name='TP')
+    x_test=np.array([140]).reshape(-1,1)
+    y_pred=[]
+
+    X=np.array(df['No. of responses'])
+    Y=[np.array(df['DIP']),np.array(df['AIS']),np.array(df['SEO']),np.array(df['SA']),np.array(df['UE']),np.array(df['ACN'])]
+
+    titles=['DIP','AIS','SEO','SA','UE','ACN']
+    x1=X.tolist()
+    num=0
+    for y in Y:
+        reg = LinearRegression().fit(X.reshape(-1,1), y)
+        p=reg.predict(x_test)
+        y_pred.append(round(p[0],0))
+        y1=y.tolist()
+
+        fig = plt.figure(figsize=(6,4))
+        plt.plot(x1, y1,'o-b')
+        
+        plt.title(titles[num])
+
+        plt.xticks([15,25,35,45,55,65])
+        num+=1
+
+        plt.xlabel('Total No. of Students')
+        plt.ylabel('Students in favour')
+        
+        for i,j in zip(X,y):
+            plt.annotate(str(round(j,0)),xy=(i,j))
+        plt.tight_layout()
+        fig.savefig('static/images/tp/'+titles[num-1]+'.jpg')
+    
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.bar(titles, y_pred, color = 'b')
+    plt.yticks([0,50,100])
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x()+(p.get_width()/2), p.get_height() * 1.01))
+
+    plt.xlabel('Subject')
+    plt.ylabel('Expected no. of Students in favour')
+    plt.title('Prediction for total 140 students')
+        
+    plt.tight_layout()
+    fig.savefig('static/images/tp/analysis.jpg')
+
+    included=sum(y_pred)
+    missed=(140-included)
+    
+    return render(request, 'tp_home.html',{'missed':missed,'included':included,'titles':titles})
+
+def fop(request):
+    df=pd.read_excel('static/excel/IDC.xlsx',sheet_name='FoP')
+    x_test=np.array([140]).reshape(-1,1)
+    y_pred=[]
+
+    X=np.array(df['No. of responses'])
+    Y=[np.array(df['DIP']),np.array(df['AIS']),np.array(df['SEO']),np.array(df['SA']),np.array(df['UE']),np.array(df['ACN'])]
+
+    titles=['DIP','AIS','SEO','SA','UE','ACN']
+    x1=X.tolist()
+    num=0
+    for y in Y:
+        reg = LinearRegression().fit(X.reshape(-1,1), y)
+        p=reg.predict(x_test)
+        y_pred.append(round(p[0],0))
+        y1=y.tolist()
+
+        fig = plt.figure(figsize=(6,4))
+        plt.plot(x1, y1,'o-b')
+        
+        plt.title(titles[num])
+
+        plt.xticks([15,25,35,45,55,65])
+        num+=1
+
+        plt.xlabel('Total No. of Students')
+        plt.ylabel('Students in favour')
+        
+        for i,j in zip(X,y):
+            plt.annotate(str(round(j,0)),xy=(i,j))
+        plt.tight_layout()
+        fig.savefig('static/images/fop/'+titles[num-1]+'.jpg')
+    
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.bar(titles, y_pred, color = 'b')
+    plt.yticks([0,50,100])
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x()+(p.get_width()/2), p.get_height() * 1.01))
+
+    plt.xlabel('Subject')
+    plt.ylabel('Expected no. of Students in favour')
+    plt.title('Prediction for total 140 students')
+        
+    plt.tight_layout()
+    fig.savefig('static/images/fop/analysis.jpg')
+
+    included=sum(y_pred)
+    missed=(140-included)
+    
+    return render(request, 'fop_home.html',{'missed':missed,'included':included,'titles':titles})
+
+def fip(request):
+    df=pd.read_excel('static/excel/IDC.xlsx',sheet_name='FiP')
+    x_test=np.array([140]).reshape(-1,1)
+    y_pred=[]
+
+    X=np.array(df['No. of responses'])
+    Y=[np.array(df['DIP']),np.array(df['AIS']),np.array(df['SEO']),np.array(df['SA']),np.array(df['UE']),np.array(df['ACN'])]
+
+    titles=['DIP','AIS','SEO','SA','UE','ACN']
+    x1=X.tolist()
+    num=0
+    for y in Y:
+        reg = LinearRegression().fit(X.reshape(-1,1), y)
+        p=reg.predict(x_test)
+        y_pred.append(round(p[0],0))
+        y1=y.tolist()
+
+        fig = plt.figure(figsize=(6,4))
+        plt.plot(x1, y1,'o-b')
+        
+        plt.title(titles[num])
+
+        plt.xticks([15,25,35,45,55,65])
+        num+=1
+
+        plt.xlabel('Total No. of Students')
+        plt.ylabel('Students in favour')
+        
+        for i,j in zip(X,y):
+            plt.annotate(str(round(j,0)),xy=(i,j))
+        plt.tight_layout()
+        fig.savefig('static/images/fip/'+titles[num-1]+'.jpg')
+    
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.bar(titles, y_pred, color = 'b')
+    plt.yticks([0,50,100])
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x()+(p.get_width()/2), p.get_height() * 1.01))
+
+    plt.xlabel('Subject')
+    plt.ylabel('Expected no. of Students in favour')
+    plt.title('Prediction for total 140 students')
+        
+    plt.tight_layout()
+    fig.savefig('static/images/fip/analysis.jpg')
+
+    included=sum(y_pred)
+    missed=(140-included)
+    
+    return render(request, 'fip_home.html',{'missed':missed,'included':included,'titles':titles})
+
+def sip(request):
+    df=pd.read_excel('static/excel/IDC.xlsx',sheet_name='SiP')
+    x_test=np.array([140]).reshape(-1,1)
+    y_pred=[]
+
+    X=np.array(df['No. of responses'])
+    Y=[np.array(df['DIP']),np.array(df['AIS']),np.array(df['SEO']),np.array(df['SA']),np.array(df['UE']),np.array(df['ACN'])]
+
+    titles=['DIP','AIS','SEO','SA','UE','ACN']
+    x1=X.tolist()
+    num=0
+    for y in Y:
+        reg = LinearRegression().fit(X.reshape(-1,1), y)
+        p=reg.predict(x_test)
+        y_pred.append(round(p[0],0))
+        y1=y.tolist()
+
+        fig = plt.figure(figsize=(6,4))
+        plt.plot(x1, y1,'o-b')
+        
+        plt.title(titles[num])
+
+        plt.xticks([15,25,35,45,55,65])
+        num+=1
+
+        plt.xlabel('Total No. of Students')
+        plt.ylabel('Students in favour')
+        
+        for i,j in zip(X,y):
+            plt.annotate(str(round(j,0)),xy=(i,j))
+        plt.tight_layout()
+        fig.savefig('static/images/sip/'+titles[num-1]+'.jpg')
+    
+    fig, ax = plt.subplots(figsize=(12,4))
+    plt.bar(titles, y_pred, color = 'b')
+    plt.yticks([0,50,100])
+    for p in ax.patches:
+        ax.annotate(str(p.get_height()), (p.get_x()+(p.get_width()/2), p.get_height() * 1.01))
+
+    plt.xlabel('Subject')
+    plt.ylabel('Expected no. of Students in favour')
+    plt.title('Prediction for total 140 students')
+        
+    plt.tight_layout()
+    fig.savefig('static/images/sip/analysis.jpg')
+
+    included=sum(y_pred)
+    missed=(140-included)
+    
+    return render(request, 'sip_home.html',{'missed':missed,'included':included,'titles':titles})
